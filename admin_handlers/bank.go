cd69160f830b0d26ffc4bda695f0d00b1d6f2af9
Christian <cwinata@ayannah.com>
2019-11-01 15:10:13 +0700

    add bank type

--- admin_handlers/bank.go
+++ admin_handlers/bank.go
@@ -1,14 +1,18 @@
 package admin_handlers
 
 import (
+	"asira_lender/asira"
 	"asira_lender/models"
 	"fmt"
+	"log"
+	"math"
 	"net/http"
 	"strconv"
 	"strings"
 
 	"github.com/labstack/echo"
 	"github.com/thedevsaddam/govalidator"
+	"gitlab.com/asira-ayannah/basemodel"
 )
 
 // type BankPayload struct {
@@ -28,28 +32,82 @@ import (
 func BankList(c echo.Context) error {
 	defer c.Request().Body.Close()
 
-	// pagination parameters
-	rows, err := strconv.Atoi(c.QueryParam("rows"))
-	page, err := strconv.Atoi(c.QueryParam("page"))
-	order := strings.Split(c.QueryParam("orderby"), ",")
-	sort := strings.Split(c.QueryParam("sort"), ",")
+	db := asira.App.DB
 
-	// filters
-	name := c.QueryParam("name")
-	id := customSplit(c.QueryParam("id"), ",")
+	type BankSelect struct {
+		models.Bank
+		BankTypeName string `json:"bank_type_name"`
+	}
+	var (
+		totalRows int
+		offset    int
+		rows      int
+		page      int
+		banks     []BankSelect
+	)
+
+	// pagination parameters
+	if c.QueryParam("rows") != "all" {
+		rows, _ = strconv.Atoi(c.QueryParam("rows"))
+		page, _ = strconv.Atoi(c.QueryParam("page"))
+		if page <= 0 {
+			page = 1
+		}
+		if rows <= 0 {
+			rows = 25
+		}
+		offset = (page * rows) - rows
+	}
 
 	type Filter struct {
 		ID   []string `json:"id"`
 		Name string   `json:"name" condition:"LIKE"`
 	}
 
-	bank := models.Bank{}
-	result, err := bank.PagedFindFilter(page, rows, order, sort, &Filter{
-		ID:   id,
-		Name: name,
-	})
+	db = db.Table("banks b").
+		Select("b.*, bt.name as bank_type_name").
+		Joins("INNER JOIN bank_types bt ON b.type = bt.id")
+
+	if name := c.QueryParam("name"); len(name) > 0 {
+		db = db.Where("b.name LIKE ?", name)
+	}
+	if id := customSplit(c.QueryParam("id"), ","); len(id) > 0 {
+		db = db.Where("b.id IN (?)", id)
+	}
+
+	if order := strings.Split(c.QueryParam("orderby"), ","); len(order) > 0 {
+		if sort := strings.Split(c.QueryParam("sort"), ","); len(sort) > 0 {
+			for k, v := range order {
+				q := v
+				if len(sort) > k {
+					value := sort[k]
+					if strings.ToUpper(value) == "ASC" || strings.ToUpper(value) == "DESC" {
+						q = v + " " + strings.ToUpper(value)
+					}
+				}
+				db = db.Order(q)
+			}
+		}
+	}
+
+	if rows > 0 && offset > 0 {
+		db = db.Limit(rows).Offset(offset)
+	}
+	err := db.Find(&banks).Count(&totalRows).Error
 	if err != nil {
-		return returnInvalidResponse(http.StatusInternalServerError, err, "pencarian tidak ditemukan")
+		log.Println(err)
+	}
+
+	lastPage := int(math.Ceil(float64(totalRows) / float64(rows)))
+
+	result := basemodel.PagedFindResult{
+		TotalData:   totalRows,
+		Rows:        rows,
+		CurrentPage: page,
+		LastPage:    lastPage,
+		From:        offset + 1,
+		To:          offset + rows,
+		Data:        banks,
 	}
 
 	return c.JSON(http.StatusOK, result)
