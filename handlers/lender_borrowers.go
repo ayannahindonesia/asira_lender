313f201ecd7bb05429aa64618cdb59244026e7f3
Christian <cwinata@ayannah.com>
2019-11-25 15:44:18 +0700

    detail

--- handlers/lender_borrowers.go
+++ handlers/lender_borrowers.go
@@ -215,7 +215,7 @@ func LenderBorrowerListDetail(c echo.Context) error {
 	borrower := BorrowerSelect{}
 
 	err = db.Table("borrowers b").
-		Select("b.*, a.category, ba.name as bank_name, a.name as agent_name, ap.name as agent_provider_name").
+		Select("b.*, a.category, ba.name as bank_name, a.name as agent_name, ap.name as agent_provider_name, (SELECT COUNT(id) FROM loans l WHERE l.owner = b.id) as loan_count, CASE WHEN (SELECT COUNT(id) FROM loans l WHERE l.owner = b.id AND status = ?) > 1 THEN ? ELSE ? END as loan_status", "active", "active", "inactive").
 		Joins("LEFT JOIN agents a ON b.agent_id = a.id").
 		Joins("LEFT JOIN banks ba ON ba.id = b.bank").
 		Joins("LEFT JOIN agent_providers ap ON a.agent_provider = ap.id").
