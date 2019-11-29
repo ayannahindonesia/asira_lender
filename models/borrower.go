e27c0a41172ceb8846a620ff17a6b7568ec2e6bb
Christian <cwinata@ayannah.com>
2019-11-29 16:43:36 +0700

    change type

--- models/borrower.go
+++ models/borrower.go
@@ -15,9 +15,9 @@ type (
 		Fullname             string        `json:"fullname" gorm:"column:fullname;type:varchar(255);not_null" csv:"fullname"`
 		Gender               string        `json:"gender" gorm:"column:gender;type:varchar(1);not null csv:"gender"`
 		IdCardNumber         string        `json:"idcard_number" gorm:"column:idcard_number;type:varchar(255);unique;not null" csv:"idcard_number"`
-		IdCardImage          string        `json:"idcard_image" gorm:"column:idcard_image;type:varchar(255)" csv:"idcard_image"`
+		IdCardImage          sql.NullInt64 `json:"idcard_image" gorm:"column:idcard_image;type:varchar(255)" csv:"idcard_image"`
 		TaxIDnumber          string        `json:"taxid_number" gorm:"column:taxid_number;type:varchar(255)" csv:"taxid_number"`
-		TaxIDImage           string        `json:"taxid_image" gorm:"column:taxid_image;type:varchar(255)" csv:"taxid_image"`
+		TaxIDImage           sql.NullInt64 `json:"taxid_image" gorm:"column:taxid_image;type:varchar(255)" csv:"taxid_image"`
 		Email                string        `json:"email" gorm:"column:email;type:varchar(255);unique" csv:"email"`
 		Birthday             time.Time     `json:"birthday" gorm:"column:birthday;not null" csv:"birthday"`
 		Birthplace           string        `json:"birthplace" gorm:"column:birthplace;type:varchar(255);not null" csv:"birthplace"`
