439d9ab24d708b31a655a35e1d6c90748ca4aa42
royadv <radventus@ayannah.com>
2020-02-27 13:59:32 +0700

    implement bank_id for showing null or not null borrower's bank

--- adminhandlers/admin_borrower.go
+++ adminhandlers/admin_borrower.go
@@ -63,7 +63,9 @@ func BorrowerGetAll(c echo.Context) error {
 		Joins("LEFT JOIN banks ba ON ba.id = borrowers.bank").
 		Joins("LEFT JOIN agent_providers ap ON a.agent_provider = ap.id")
 
+	bankID := c.QueryParam("bank_id")
 	accountNumber := c.QueryParam("account_number")
+
 	if status := c.QueryParam("status"); len(status) > 0 {
 		db = db.Where("borrowers.status = ?", strings.ToLower(status))
 	} else {
@@ -85,6 +87,15 @@ func BorrowerGetAll(c echo.Context) error {
 				db = db.Where("borrowers.bank_accountnumber = ?", "")
 			} else if accountNumber == "not null" {
 				db = db.Where("borrowers.bank_accountnumber != ?", "")
+			}
+		}
+
+		//filter bank id for boorrwers mobile if bankID == null
+		if len(bankID) > 0 {
+			if bankID == "null" {
+				db = db.Where("borrowers.bank = ?", 0)
+			} else if bankID == "not null" {
+				db = db.Where("borrowers.bank != ?", 0)
 			}
 		}
 
@@ -115,6 +126,15 @@ func BorrowerGetAll(c echo.Context) error {
 				db = db.Where("borrowers.bank_accountnumber != ?", "")
 			} else {
 				db = db.Where("borrowers.bank_accountnumber LIKE ?", "%"+strings.ToLower(accountNumber)+"%")
+			}
+		}
+
+		//filter bank id for boorrwers mobile if bankID == null
+		if len(bankID) > 0 {
+			if bankID == "null" {
+				db = db.Where("borrowers.bank = ?", 0)
+			} else if bankID == "not null" {
+				db = db.Where("borrowers.bank != ?", 0)
 			}
 		}
 	}
